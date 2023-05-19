116bd44a057e3c956492a34afc6a8772846e3153
hiteshr <hitesh.regar@aptportfolio.com>
2023-05-19 18:11:35 +0530

    document ++

--- src/components/DocumentView/DocumentView.tsx
+++ src/components/DocumentView/DocumentView.tsx
@@ -1,7 +1,123 @@
-import { Box } from "@mui/material";
-import * as React from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardMedia,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  Typography,
+} from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../store/app/hooks";
+import {
+  Document,
+  selectDocuments,
+  toggleFavorite,
+  updateDocumentName,
+} from "../../store/features/document/documentSlice";
+
+import DoneIcon from "@mui/icons-material/Done";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import React, { ChangeEvent } from "react";
+import AddDocumentDialog from "./AddDocumentDialog";
+
+interface IDocumentProps {
+  document: Document;
+}
+
+const IDocumnet = ({ document }: IDocumentProps): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const [isRename, setIsRename] = React.useState(false);
+  const [name, setName] = React.useState(document.name);
+
+  const isRenameToggle = () => {
+    setIsRename(!isRename);
+  };
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => setName(event.target.value);
+
+  const _handleKeyDown = (e: { key: string }) => {
+    if (e.key === "Enter") {
+      handleUdateDocumentName();
+    }
+  };
+
+  const handleUdateDocumentName = () => {
+    dispatch(
+      updateDocumentName({
+        id: document.id,
+        name: name,
+      })
+    );
+    isRenameToggle();
+  };
+
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image={`https://source.unsplash.com/random?bitcoin,${document.id}`}
+      />
+      <CardActions sx={{ justifyContent: "flex-end" }}>
+        {!isRename ? (
+          <Typography onClick={() => isRenameToggle()}>
+            {document.name}
+          </Typography>
+        ) : (
+          <Input
+            size="small"
+            value={name}
+            onChange={handleChange}
+            onBlur={() => {
+              if (name === document.name) {
+                isRenameToggle();
+              }
+            }}
+            onKeyDown={_handleKeyDown}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => handleUdateDocumentName()}
+                  edge="end"
+                >
+                  <DoneIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        )}
+
+        <IconButton
+          onClick={() => dispatch(toggleFavorite(document.id))}
+          color={document.is_favorite ? "secondary" : "primary"}
+        >
+          {document.is_favorite ? <StarIcon /> : <StarBorderIcon />}
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
 
 const DocumentView = (): JSX.Element => {
-  return <Box> Documents</Box>;
+  const documents = useAppSelector(selectDocuments);
+  return (
+    <Box sx={{ height: "92vh", overflow: "auto" }}>
+      <Grid container spacing={1}>
+        {documents.map((document: Document) => (
+          <Grid item xs={3} id={document.id}>
+            <IDocumnet document={document} />
+          </Grid>
+        ))}
+      </Grid>
+      <AddDocumentDialog />
+    </Box>
+  );
 };
 export default DocumentView;
--- src/store/app/store.ts
+++ src/store/app/store.ts
@@ -1,22 +1,25 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import userReducer from "../features/user/userSlice";
+import documentReducer from "../features/document/documentSlice";
 import { postApi } from "../services/post";
-import storage from "redux-persist/lib/storage";
+// import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 import { combineReducers } from "redux";
 import persistStore from "redux-persist/es/persistStore";
 import { photosApi } from "../services/photos";
+import createIdbStorage from "@piotr-cz/redux-persist-idb-storage";
 
 const persistConfig = {
   key: "root",
-  storage,
-  whitelist: ["users"],
+  storage: createIdbStorage({ name: "myApp", storeName: "keyval" }),
+  whitelist: ["users", "documents"],
 };
 
 const reducers = combineReducers({
   counter: counterReducer,
   users: userReducer,
+  documents: documentReducer,
   [postApi.reducerPath]: postApi.reducer,
   [photosApi.reducerPath]: photosApi.reducer,
 });
